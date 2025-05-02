6b596a16ff374793e06b25f9ed765487503e5949
Shopin <nikshopin@gmail.com>
2025-05-02 12:43:06 +0300

    task 2 inprocess

--- src/components/MyComponents.tsx
+++ src/components/MyComponents.tsx
@@ -5,13 +5,19 @@ type MyComponentsType = {arrays: ICourse};
 
 const MyComponents: FC <MyComponentsType> = ({arrays}): Element=> {
     return (
-            {
-                arrays.map((value)=>{
-                    <div className='text-3xl font-bold underline'>
-                        value.title
-                    </div>
-                })
-            }
+            <div>
+                {
+                        arrays.map((value, index)=>{
+                            return(
+                            <p className='text-3xl font-bold underline'>
+                                value.index value.title . value.mounth
+                            </p>
+
+                                )
+                        })
+
+                }
+            </div>
     )
 }
 
