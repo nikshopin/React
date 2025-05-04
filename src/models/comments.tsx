25b94faed1438cd0eb9f10e892abe9d71aa1202e
Shopin <nikshopin@gmail.com>
2025-05-04 23:00:36 +0300

    task 3 in process

--- src/models/comments.tsx
+++ src/models/comments.tsx
@@ -1,7 +1,13 @@
-export interface IComment  {
-  postId: number;
+export interface User {
   id: number;
-  name: string;
-  email: string;
+  username: string;
+  fullName: string;
+}
+
+export interface IComment {
+  id: number;
   body: string;
+  postId: number;
+  likes: number;
+  user: User;
 }
