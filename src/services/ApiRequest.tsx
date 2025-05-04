97b2ac167391c7dafdfb26276d1ecc989703f584
Shopin <nikshopin@gmail.com>
2025-05-04 22:06:55 +0300

    task 3 in process

--- src/services/ApiRequest.tsx
+++ src/services/ApiRequest.tsx
@@ -3,6 +3,5 @@ import {IPost} from "../models/posts.tsx";
 
 export const ApiRequest =async(url:string):Promise< IPost[] > => {
     return await fetch(url)
-    // return await fetch('https://jsonplaceholder.typicode.com/posts')
         .then(res => res.json());
 }
